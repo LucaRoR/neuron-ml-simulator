Beta: Add app icon. Fix various numerical bugs. Remove unused files. Add meaningful preset parameters.

ml_app/gui/phaseplane_canvas.py
@@ -196,11 +196,15 @@ class PhasePlaneCanvas(QWidget): #render the (u,w)-plane. It contains nullclines
                 DW[i,j] = g(u, w, par)
         
         
-        #we normalize the vectors to avoid having a few arrows clogging up the whole space
+        #we normalize the vectors to avoid having a few arrows clogging up the whole space. We prevent norm == 0
         norm = np.sqrt(DU ** 2 + DW ** 2)
-        norm[norm == 0.0] = 1.0
-        DU_n = DU / norm
-        DW_n = DW / norm
+        DU_n = np.zeros_like(DU)
+        DW_n = np.zeros_like(DW)
+
+        mask = norm > 0
+
+        DU_n[mask] = DU[mask] / norm[mask]
+        DW_n[mask] = DW[mask] / norm[mask]
 
         self._ax.quiver(U, W, DU_n, DW_n, angles="xy", scale_units="xy", scale=1/6, width=0.003, alpha=0.4, zorder=1)
 

ml_app/model/ml_equations.py
@@ -1,30 +1,29 @@
 import numpy as np
 from .parameters import MLParameters
 
+#We avoid division by zero
+def _safe_slope(n: float, eps: float = 1e-3):
+    if abs(n) < eps:
+        return eps
+    return n
+
 #Steady-state functions and time constants
 def meff_inf(u: float, par: MLParameters) -> float:
-    return 0.5 * (1 + np.tanh((u - par.V1_meff) / par.V2_meff))
+    V2_meff = _safe_slope(par.V2_meff)
+    return 0.5 * (1 + np.tanh((u - par.V1_meff) / V2_meff))
 
 def w_inf(u: float, par: MLParameters) -> float:
-    return 0.5 * (1 + np.tanh((u - par.V3_w) / par.V4_w))
+    V4_w = _safe_slope(par.V4_w)
+    return 0.5 * (1 + np.tanh((u - par.V3_w) / V4_w))
 
 def tau_w(u: float, par: MLParameters) -> float:
-    return 1 / (2 * par.Phi_w * np.cosh((u - par.V3_w) / par.V4_w))
+    V4_w = _safe_slope(par.V4_w)
+    return 1 / (2 * par.Phi_w * np.cosh((u - par.V3_w) / V4_w))
 
-def m_inf(u: float, par: MLParameters) -> float:
-    return 0.5 * (1 + np.tanh((u - par.V1_m) / par.V2_m))
-
-def h_inf(u: float, par: MLParameters) -> float:
-    return 0.5 * (1 - np.tanh((u - par.V1_h) / par.V2_h))
 
 #Ionic currents
-def na_activation(u, par):
-    if par.meff_toggle:
-        return meff_inf(u, par)
-    return (m_inf(u, par) ** 3) * h_inf(u, par)
-
 def I_Na(u: float, par:MLParameters) -> float:
-    return par.g_Na * na_activation(u, par) * (u - par.E_Na)
+    return par.g_Na * meff_inf(u, par) * (u - par.E_Na)
 
 def I_K(u: float, w: float, par:MLParameters) -> float:
     return par.g_K * w * (u - par.E_K)